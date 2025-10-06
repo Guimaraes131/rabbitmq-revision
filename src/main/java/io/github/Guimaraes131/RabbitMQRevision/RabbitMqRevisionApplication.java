0cf29b49d0f364f8f9bbe26c527e34190140f8c3
Guilherme <guilhermegpereira26@hotmail.com>
2025-10-06 19:50:18 -0300

    chore: first commit with compose config

---
+++ src/main/java/io/github/Guimaraes131/RabbitMQRevision/RabbitMqRevisionApplication.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.RabbitMQRevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqRevisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Guimaraes131/RabbitMQRevision/RabbitMqRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.RabbitMQRevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqRevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
